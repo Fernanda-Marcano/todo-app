8b3c703358c5c4a6d768e10b0c0ddfa63ebff540
Fernanda-Marcano <marcanofernanda17@gmail.com>
2024-06-03 10:46:05 -0400

    Creando la estructura del formulario de creación de tareas

--- tareas/forms.py
+++ tareas/forms.py
@@ -1,1 +1,20 @@
-from django.forms import ModelForm
+from django import forms
+from django.forms import TextInput
+from .models import Tarea
+
+class TareaForm(forms.ModelForm):
+    class Meta:
+        model = Tarea
+        exclude = ['fecha_inicial']
+        labels = {
+            'titulo':'Título', 
+            'descripcion':'Descripción',
+            'prioridad':'Prioridad',
+            'fecha_final':'Fecha final'
+        }
+        error_messages = {
+            'titulo':'Este campo no debe estar vacío'
+        }
+        widgets = {
+            'prioridad':TextInput(attrs={'type':'number', "min":1, 'max':7})
+        }
