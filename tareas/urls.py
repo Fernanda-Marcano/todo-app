693ffe6625d499916d4945e98e93b70680ef6c20
Fernanda-Marcano <marcanofernanda17@gmail.com>
2024-06-08 16:32:48 -0400

    Se agregó la ventana del detalle de la tarea Agregando el manejo de mensajes a las vistas, y validando los posibles eventos que se puedan generar Modificando el archivo urls.py para agregar la ruta de los detalles Se creo un archivo de estilos, para manejar la card de presentación del detalle de la tarea

--- tareas/urls.py
+++ tareas/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('crear/', views.crear_tarea, name='crear-tarea'),
     path('lista/', views.lista_tarea, name='lista-tarea'), 
     path('editar/<int:id>/', views.editar_tarea, name='editar-tarea'),
+    path('detalle/<int:id>/', views.detalle_tarea, name='detalle-tarea'),
     path('eliminar/<int:id>/', views.eliminar_tarea, name='eliminar-tarea'),
 ]
--- tareas/views.py
+++ tareas/views.py
@@ -1,55 +1,80 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
 from .models import Tarea
 from .forms import TareaForm
 
 
 def crear_tarea(request):
-    if request.method == 'POST':
-        form = TareaForm(request.POST or None)
-        if form.is_valid():
-            form.save()
-            return redirect(to='lista-tarea')
+    try:
+        if request.method == 'POST':
+            form = TareaForm(request.POST or None)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'La tarea se ha registrado correctamente')
+                return redirect(to='lista-tarea')
+            else:
+                messages.error(request, 'Ha ocurrido un error con los datos del formulario. Por favor verifique que este correcto')
+                return redirect(to='crear-tarea')
         else:
-            return HttpResponse('Error al crear la tarea')
-    else:
-        form = TareaForm()
-        context = {
-            'form':form,
-        }
-        return render(request, 'tarea/crear_tarea.html', context)
+            form = TareaForm()
+            context = {
+                'form':form,
+            }
+            return render(request, 'tarea/crear_tarea.html', context)
+    except ValueError as e:
+        print(f'Error {e}')
 
 
 def lista_tarea(request):
-    form = Tarea.objects.all()
-    context = {
-        'forms':form,
-    }
-    return render(request, 'tarea/lista_tarea.html', context)
+    try:
+        form = Tarea.objects.all()
+        context = {
+            'forms':form,
+        }
+        return render(request, 'tarea/lista_tarea.html', context)
+    except ValueError as e:
+        print(f'Error {e}')
 
 
 def editar_tarea(request, id):
-    id_tarea = Tarea.objects.get(id=id)
-    form = TareaForm(request.POST or None, instance=id_tarea)
-    if form.is_valid():
-        form.save()
-        return redirect(to='lista-tarea')
-    else:
-        HttpResponse('Error')
-    if request.method == 'GET':
-        form = TareaForm(instance=id_tarea)
+    try:
+        id_tarea = Tarea.objects.get(id=id)
+        form = TareaForm(request.POST or None, instance=id_tarea)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Tarea editada correctamente')
+            return redirect(to='lista-tarea')
+        else:
+            messages.error(request, 'Ha ocurrido un error')
+        if request.method == 'GET':
+            form = TareaForm(instance=id_tarea)
+            context = {
+                'form':form, 
+                'id_tarea':id_tarea,
+            }
+            return render(request, 'tarea/editar_tarea.html', context)
+    except ValueError as e:
+        print(f'Error {e}')
+
+
+def detalle_tarea(request, id):
+    try:
+        id_tarea = Tarea.objects.get(id=id)
         context = {
-            'form':form, 
-            'id_tarea':id_tarea,
+            'id_tarea':id_tarea
         }
-        return render(request, 'tarea/editar_tarea.html', context)
+        return render(request, 'tarea/detalle_tarea.html', context)
+    except ValueError as e:
+        print(f'Ha ocurrido un error {e}')
 
 
 def eliminar_tarea(request, id):
     try:
         id_tarea = Tarea.objects.get(id=id)
         id_tarea.delete()
+        messages.success(request, 'Tarea eliminada correctamente')
         return redirect(to='lista-tarea')
-    except Exception as e:
+    except ValueError as e:
         print(f'Ha ocurrido un error {e}')
 
